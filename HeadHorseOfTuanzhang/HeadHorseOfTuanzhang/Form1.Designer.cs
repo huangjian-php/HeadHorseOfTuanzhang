9a0da4cc67a9512640295c264d6f183bc07469f2
黄健 <150588337@qq.com>
2016-09-10 00:19:01 +0800

    add pic

--- HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.Designer.cs
+++ HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_num = new System.Windows.Forms.TextBox();
+            this.lbl_num = new System.Windows.Forms.Label();
+            this.btn_ok = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(86, 38);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(29, 12);
+            this.lbl_name.TabIndex = 0;
+            this.lbl_name.Text = "名字";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(147, 35);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(100, 21);
+            this.tb_name.TabIndex = 1;
+            // 
+            // tb_num
+            // 
+            this.tb_num.Location = new System.Drawing.Point(147, 79);
+            this.tb_num.Name = "tb_num";
+            this.tb_num.Size = new System.Drawing.Size(100, 21);
+            this.tb_num.TabIndex = 3;
+            // 
+            // lbl_num
+            // 
+            this.lbl_num.AutoSize = true;
+            this.lbl_num.Location = new System.Drawing.Point(86, 82);
+            this.lbl_num.Name = "lbl_num";
+            this.lbl_num.Size = new System.Drawing.Size(29, 12);
+            this.lbl_num.TabIndex = 2;
+            this.lbl_num.Text = "编号";
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(147, 129);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.TabIndex = 4;
+            this.btn_ok.Text = "生成";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 418);
+            this.ClientSize = new System.Drawing.Size(393, 223);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.tb_num);
+            this.Controls.Add(this.lbl_num);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.lbl_name);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_num;
+        private System.Windows.Forms.Label lbl_num;
+        private System.Windows.Forms.Button btn_ok;
     }
 }
 
--- HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.cs
+++ HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace HeadHorseOfTuanzhang
 {
@@ -14,6 +15,56 @@ namespace HeadHorseOfTuanzhang
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public bool SaveImageForSpecifiedQuality(System.Drawing.Image sourceImage, string savePath, int imageQualityValue)
+        {
+            //以下代码为保存图片时，设置压缩质量
+            EncoderParameters encoderParameters = new EncoderParameters();
+            EncoderParameter encoderParameter = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, imageQualityValue);
+            encoderParameters.Param[0] = encoderParameter;
+            try
+            {
+                ImageCodecInfo[] ImageCodecInfoArray = ImageCodecInfo.GetImageEncoders();
+                ImageCodecInfo jpegImageCodecInfo = null;
+                for (int i = 0; i < ImageCodecInfoArray.Length; i++)
+                {
+                    if (ImageCodecInfoArray[i].FormatDescription.Equals("JPEG"))
+                    {
+                        jpegImageCodecInfo = ImageCodecInfoArray[i];
+                        break;
+                    }
+                }
+                sourceImage.Save(savePath, jpegImageCodecInfo, encoderParameters);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            string name_str = tb_name.Text;
+            string num_str = tb_num.Text;
+            Image image = Image.FromFile("base.jpg");
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                StringFormat format = new StringFormat();
+                format.LineAlignment = StringAlignment.Center;  // 更正： 垂直居中
+                format.Alignment = StringAlignment.Center;      // 水平居中
+
+                RectangleF rf_name = new Rectangle(new Point(183, 1879), new Size(693, 246));
+                RectangleF rf_num = new Rectangle(new Point(195, 2125), new Size(693, 82));
+                g.DrawString(name_str, new Font("微软雅黑", 200),
+                    Brushes.Black, rf_name, format);
+                g.DrawString(num_str, new Font("宋体", 50),
+                    Brushes.DimGray, rf_num, format);
+                g.Flush();
+            }
+            //image.Save(name_str + ".jpg");
+            this.SaveImageForSpecifiedQuality(image, "已p\\"+name_str + ".jpg", 100);
         }
     }
 }
