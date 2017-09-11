99e5616584d869b2ec503945d8d6044148b8bfb7
JamHuang <150588337@qq.com>
2017-09-12 03:28:50 +0800

    edit

--- HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.Designer.cs
+++ HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.Designer.cs
@@ -33,43 +33,54 @@
             this.tb_num = new System.Windows.Forms.TextBox();
             this.lbl_num = new System.Windows.Forms.Label();
             this.btn_ok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.ofd_excel = new System.Windows.Forms.OpenFileDialog();
+            this.tb_file = new System.Windows.Forms.TextBox();
+            this.cb_pic = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbl_name
             // 
             this.lbl_name.AutoSize = true;
-            this.lbl_name.Location = new System.Drawing.Point(86, 38);
+            this.lbl_name.Location = new System.Drawing.Point(-3, 0);
             this.lbl_name.Name = "lbl_name";
             this.lbl_name.Size = new System.Drawing.Size(29, 12);
             this.lbl_name.TabIndex = 0;
             this.lbl_name.Text = "名字";
+            this.lbl_name.Visible = false;
             // 
             // tb_name
             // 
-            this.tb_name.Location = new System.Drawing.Point(147, 35);
+            this.tb_name.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.tb_name.Location = new System.Drawing.Point(58, -3);
             this.tb_name.Name = "tb_name";
             this.tb_name.Size = new System.Drawing.Size(100, 21);
             this.tb_name.TabIndex = 1;
+            this.tb_name.Visible = false;
             // 
             // tb_num
             // 
-            this.tb_num.Location = new System.Drawing.Point(147, 79);
+            this.tb_num.Location = new System.Drawing.Point(58, 41);
             this.tb_num.Name = "tb_num";
             this.tb_num.Size = new System.Drawing.Size(100, 21);
             this.tb_num.TabIndex = 3;
+            this.tb_num.Visible = false;
             // 
             // lbl_num
             // 
             this.lbl_num.AutoSize = true;
-            this.lbl_num.Location = new System.Drawing.Point(86, 82);
+            this.lbl_num.Location = new System.Drawing.Point(-3, 44);
             this.lbl_num.Name = "lbl_num";
             this.lbl_num.Size = new System.Drawing.Size(29, 12);
             this.lbl_num.TabIndex = 2;
             this.lbl_num.Text = "编号";
+            this.lbl_num.Visible = false;
             // 
             // btn_ok
             // 
-            this.btn_ok.Location = new System.Drawing.Point(147, 129);
+            this.btn_ok.Location = new System.Drawing.Point(152, 143);
             this.btn_ok.Name = "btn_ok";
             this.btn_ok.Size = new System.Drawing.Size(75, 23);
             this.btn_ok.TabIndex = 4;
@@ -77,18 +88,77 @@
             this.btn_ok.UseVisualStyleBackColor = true;
             this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "表格";
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(305, 68);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(75, 23);
+            this.btn_select.TabIndex = 6;
+            this.btn_select.Text = "选择表格";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // ofd_excel
+            // 
+            this.ofd_excel.Filter = "Excel文件|*.xls;*.xlsx||*.*";
+            this.ofd_excel.Title = "请选择Excel";
+            // 
+            // tb_file
+            // 
+            this.tb_file.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.tb_file.Location = new System.Drawing.Point(99, 68);
+            this.tb_file.Name = "tb_file";
+            this.tb_file.Size = new System.Drawing.Size(179, 21);
+            this.tb_file.TabIndex = 7;
+            // 
+            // cb_pic
+            // 
+            this.cb_pic.FormattingEnabled = true;
+            this.cb_pic.Items.AddRange(new object[] {
+            "机智云",
+            "五号空间"});
+            this.cb_pic.Location = new System.Drawing.Point(99, 105);
+            this.cb_pic.Name = "cb_pic";
+            this.cb_pic.Size = new System.Drawing.Size(121, 20);
+            this.cb_pic.TabIndex = 8;
+            this.cb_pic.Text = "机智云";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "底图";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(393, 223);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cb_pic);
+            this.Controls.Add(this.tb_file);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_ok);
             this.Controls.Add(this.tb_num);
             this.Controls.Add(this.lbl_num);
             this.Controls.Add(this.tb_name);
             this.Controls.Add(this.lbl_name);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "919";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,6 +171,12 @@
         private System.Windows.Forms.TextBox tb_num;
         private System.Windows.Forms.Label lbl_num;
         private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.OpenFileDialog ofd_excel;
+        private System.Windows.Forms.TextBox tb_file;
+        private System.Windows.Forms.ComboBox cb_pic;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.cs
+++ HeadHorseOfTuanzhang/HeadHorseOfTuanzhang/Form1.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace HeadHorseOfTuanzhang
 {
+    
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -44,27 +50,173 @@ namespace HeadHorseOfTuanzhang
             }
         }
 
-        private void btn_ok_Click(object sender, EventArgs e)
+        private void draw_pic(string prefix, string name_str, string num_str, string pic_name, RectangleF rf_name)
         {
-            string name_str = tb_name.Text;
-            string num_str = tb_num.Text;
-            Image image = Image.FromFile("base.jpg");
+            Image image = Image.FromFile(pic_name + ".jpg");
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile("PingFang Heavy.ttf");
             using (Graphics g = Graphics.FromImage(image))
             {
                 StringFormat format = new StringFormat();
                 format.LineAlignment = StringAlignment.Center;  // 更正： 垂直居中
                 format.Alignment = StringAlignment.Center;      // 水平居中
 
-                RectangleF rf_name = new Rectangle(new Point(183, 1879), new Size(693, 246));
-                RectangleF rf_num = new Rectangle(new Point(195, 2125), new Size(693, 82));
-                g.DrawString(name_str, new Font("微软雅黑", 200),
+                //RectangleF rf_name = new Rectangle(new Point(183, 1879), new Size(693, 246));
+                //RectangleF rf_name = new Rectangle(new Point(229, 812), new Size(397, 126));
+                //RectangleF rf_num = new Rectangle(new Point(195, 2125), new Size(693, 82));
+                //RectangleF rf_num = new Rectangle(new Point(229, 918), new Size(397, 42));
+                //g.DrawString(name_str, new Font("微软雅黑", 72),
+                //    Brushes.Black, rf_name, format);
+                g.DrawString(prefix + name_str, new Font(pfc.Families[0], 74),
                     Brushes.Black, rf_name, format);
-                g.DrawString(num_str, new Font("宋体", 50),
-                    Brushes.DimGray, rf_num, format);
+                //g.DrawString(num_str, new Font("宋体", 25),
+                    //Brushes.DimGray, rf_num, format);
                 g.Flush();
             }
             //image.Save(name_str + ".jpg");
-            this.SaveImageForSpecifiedQuality(image, "已p\\"+name_str + ".jpg", 100);
+            this.SaveImageForSpecifiedQuality(image, "输出\\" + num_str + name_str + ".jpg", 100);
         }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(tb_file.Text)) {
+                MessageBox.Show("请选择Excel！");
+                return;
+            }
+            string pic_name;
+            RectangleF rf_name;
+            if ("机智云" == cb_pic.Text)
+            {
+                pic_name = "gizwits";
+                rf_name = new Rectangle(new Point(125, 1010), new Size(490, 101));
+            }
+            else if ("五号空间" == cb_pic.Text)
+            {
+                pic_name = "no5";
+                rf_name = new Rectangle(new Point(125, 870), new Size(490, 101));
+            }
+            else
+            {
+                MessageBox.Show("无效的底图！");
+                return;
+            }
+            ExcelHelper eh = new ExcelHelper(tb_file.Text);
+            string[,] name_arr = eh.GetContent();
+            eh.Close();
+            int len = name_arr.GetLength(0);
+            if (len < 1) {
+                MessageBox.Show("Excel中无有效数据！");
+                return;
+            }
+            string prefix = "诚邀：";
+            for (int i = 0; i < len; i++)
+            {
+                //System.Console.WriteLine(name_arr[i, 0] + name_arr[i, 1]);
+                draw_pic(prefix, name_arr[i, 1], name_arr[i, 0], pic_name, rf_name);
+            }
+
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            if (ofd_excel.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tb_file.Text = ofd_excel.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class ExcelHelper
+    {
+        private Excel._Application excelApp;
+        private string fileName = string.Empty;
+        private Excel.WorkbookClass wbclass;
+        public ExcelHelper(string _filename)
+        {
+            excelApp = new Excel.Application();
+            object objOpt = System.Reflection.Missing.Value;
+            wbclass = (Excel.WorkbookClass)excelApp.Workbooks.Open(_filename, objOpt, false, objOpt, objOpt, objOpt, true, objOpt, objOpt, true, objOpt, objOpt, objOpt, objOpt, objOpt);
+        }
+        /**/
+        /// <summary>
+        /// 所有sheet的名称列表
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetSheetNames()
+        {
+            List<string> list = new List<string>();
+            Excel.Sheets sheets = wbclass.Worksheets;
+            string sheetNams = string.Empty;
+            foreach (Excel.Worksheet sheet in sheets)
+            {
+                list.Add(sheet.Name);
+            }
+            return list;
+        }
+        public Excel.Worksheet GetWorksheetByName(string name)
+        {
+            Excel.Worksheet sheet = null;
+            Excel.Sheets sheets = wbclass.Worksheets;
+            foreach (Excel.Worksheet s in sheets)
+            {
+                if (s.Name == name)
+                {
+                    sheet = s;
+                    break;
+                }
+            }
+            return sheet;
+        }
+        /**/
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sheetName">sheet名称</param>
+        /// <returns></returns>
+        public string[,] GetContent()
+        {
+            string sheetName = GetSheetNames()[0];
+            Excel.Worksheet sheet = GetWorksheetByName(sheetName);
+
+            //不为空的区域,列,行数目
+            //int columns = sheet.UsedRange.Columns.Count;
+            int rows = sheet.UsedRange.Rows.Count;
+            if (rows < 2) { 
+                return new string[0,0];
+            }
+            Excel.Range rng_a = sheet.Cells.get_Range("A2", "A" + rows);
+            Excel.Range rng_b = sheet.Cells.get_Range("B2", "B" + rows);
+            string[,] arry = new string[rows - 1, 2];
+            if (rows < 3) {
+                //A列
+                arry[0, 0] = rng_a.Value2.ToString();
+                //B列
+                arry[0, 1] = rng_b.Value2.ToString();
+            }
+            else {
+                object[,] arr_a = (object[,])rng_a.Value2;
+                object[,] arr_b = (object[,])rng_b.Value2;
+                for (int i = 1; i <= rows - 1; i++)
+                {
+                    //A列
+                    arry[i - 1, 0] = arr_a[i, 1].ToString();
+                    //B列
+                    arry[i - 1, 1] = arr_b[i, 1].ToString();
+                }
+            }
+            
+            return arry;
+        }
+
+        public void Close()
+        {
+            excelApp.Quit();
+            excelApp = null;
+        }
+
     }
 }
